429251c640b62f8469cbb215bb531d6e1d903eae
juaneth <euanwalker2008@gmail.com>
2024-07-15 23:18:13 +0100

    meowskos

--- setup.js
+++ setup.js
@@ -1,23 +1,55 @@
+#!/usr/bin/env node
+
 import { spinner } from "@clack/prompts";
 
-export default function setup(framework, style, apikey) {
+import * as prompts from "@clack/prompts";
+
+import { execSync } from "child_process";
+
+export default async function setup(framework, style, apikey) {
     const s = spinner();
 
-    s.start("Installing dashboardkitcore packages:");
+    s.start("Installing @dashboardkit/core packages");
 
-    const { execSync } = require("child_process");
-    let stdout = execSync("npm i @dashboardkit/core");
+    try {
+        execSync("npm i @dashboardkitjs/core");
+    } catch {
+        prompts.outro(`Error installing core package!`);
 
-    s.stop("Installed dashboardkitcore packages");
+        process.exit();
+    }
+
+    s.stop("Installed @dashboardkit/core package");
 
     switch (framework) {
         case "nodejs":
-            console.log("node!");
+            s.start("Installing @dashboardkit/nodejs package");
+
+            // TODO add node package install and boilerplate setup
+
+            s.stop("Installed @dashboardkit/nodejs packages");
+
             break;
         case "nextjs":
-            // code block
+            // TODO add nextjs package install and boilerplate setup
             break;
-        case "yah":
-            // code block
+        case "discordjs":
+            // CARLO STUFF
+
+            s.start("Installing @dashboardkit/nodejs package");
+
+            // TODO add node package install and boilerplate setup
+
+            s.stop("Installed @dashboardkit/nodejs packages");
+
+            s.start("Installing @dashboardkit/discordjs package");
+
+            // TODO add discordjs package and boilerplate setup
+
+            s.stop("Installed @dashboardkit/discordjs packages");
+
+            break;
     }
+
+    prompts.outro("Thank you for installing dashboardkit, read the docs here: https://dashkit.juaneth.dev/docs")
 }
